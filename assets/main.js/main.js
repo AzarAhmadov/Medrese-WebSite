38df3286470ea80078f5c8ceae6fb0538bded5ac
AzarAhmadov <82292818+AzarAhmadov@users.noreply.github.com>
2022-03-29 12:22:27 -0700

    banner finish

--- assets/main.js/main.js
+++ assets/main.js/main.js
@@ -1,5 +1,3 @@
-
-
 const list_li = document.querySelectorAll('.nav-list')
 const nav_link = document.querySelectorAll('.nav-link')
 
@@ -15,4 +13,28 @@ nav_link.forEach((item) => {
       document.querySelector(".nav-link.active").classList.remove("active");
       item.classList.add("active");
     });
+});
+
+var swiper1 = new Swiper(".swiper-container1", {
+  slidesPerView: 10,
+  spaceBetween: 0,
+  navigation: {
+      nextEl: ".n",
+      prevEl: ".p",
+  },
+});
+
+var swiper2 = new Swiper(".swiper-container2", {
+  loop: true,
+   slidesPerView: 1,
+        spaceBetween: 30,
+  navigation: {
+    nextEl: ".n2",
+    prevEl: ".p2",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
 });
