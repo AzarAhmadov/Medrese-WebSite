211643d31f7654e002d2938b8130be38c4e1cf03
AzarAhmadov <82292818+AzarAhmadov@users.noreply.github.com>
2022-04-02 06:28:43 -0700

    finish project

--- assets/main.js/main.js
+++ assets/main.js/main.js
@@ -1,37 +1,3 @@
-const list_li = document.querySelectorAll('.nav-list')
-const nav_link = document.querySelectorAll('.nav-link')
-
-list_li.forEach((item) => {
-    item.addEventListener("click", () => {
-      document.querySelector(".nav-list.active-li").classList.remove("active-li");
-      item.classList.add("active-li");
-    });
-});
-
-nav_link.forEach((item) => {
-    item.addEventListener("click", () => {
-      document.querySelector(".nav-link.active").classList.remove("active");
-      item.classList.add("active");
-    });
-});
-
-const mobil_menu = document.querySelector('.mobil-menu')
-const open_menu = document.querySelector('.open-menu')
-const body = document.querySelector('.body')
-const blogMobil = document.querySelector('.blog-mobil')
-const opBlog = document.querySelector('.op-mobil')
-const blogArrow = document.querySelector('.blog-arrow')
-
-opBlog.addEventListener('click', ()=>{
-  blogMobil.classList.toggle('active')
-blogMobil.classList.toggle('actives')
-blogArrow.classList.toggle('active')
-})
-
-open_menu.addEventListener('click', function(){
-  mobil_menu.classList.toggle('active')
-  body.classList.toggle('active')
-})
 
 var swiper3 = new Swiper(".swiper-container3", {
   slidesPerView: 3,
@@ -86,7 +52,6 @@ var swiper4 = new Swiper(".swiper-container4", {
   },
 });
 
-
 var swiper1 = new Swiper(".swiper-container1", {
   slidesPerView: 10,
   breakpoints: {
@@ -94,10 +59,11 @@ var swiper1 = new Swiper(".swiper-container1", {
       slidesPerView: 3,
       spaceBetween: 20,
     },
-    740: {
-      slidesPerView: 6,
+    800:{
+      slidesPerView: 7,
       spaceBetween: 20,
     },
+   
     1324: {
       slidesPerView: 10,
       spaceBetween: 0,
@@ -125,6 +91,23 @@ var swiper2 = new Swiper(".swiper-container2", {
   },
 });
 
+
+const list_li = document.querySelectorAll('.nav-list')
+const nav_link = document.querySelectorAll('.nav-link')
+
+list_li.forEach((item) => {
+    item.addEventListener("click", () => {
+      document.querySelector(".nav-list.active-li").classList.remove("active-li");
+      item.classList.add("active-li");
+    });
+});
+
+nav_link.forEach((item) => {
+    item.addEventListener("click", () => {
+      document.querySelector(".nav-link.active").classList.remove("active");
+      item.classList.add("active");
+    });
+});
 
 
 
